603e343a375be6a1139ab3f5ec72f4c3ee3ac55c
Thukten Yoezear <02230229.cst@rub.edu.bt>
2024-04-25 22:00:01 +0600

    first commit

---
+++ ty.py
@@ -0,0 +1,12 @@
+############################
+# Thukten Yoezear
+# 1ICE
+# 02230229
+############################
+# REFERENCES
+#W3school:https://www.w3schools.com/python/
+#https://realpython.com/python-rock-paper-scissors/
+############################
+# SOLUTION
+# Solution Score:50267
+############################
